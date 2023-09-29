a6f78d076804edcb23e506042cba5f2b730d5678
shilpa-mary <118098875+shilpa-mary@users.noreply.github.com>
2023-09-29 15:14:25 +0530

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,11 @@
+print("Demo of basic data types: Boolean")
+t= True
+f=False
+print("t=",t)
+print("f=",f)
+print("Data type of variable t:",type(t))
+print("Data type of variable f:",type(f))
+print("logical AND operation:",t and f)
+print("logical OR operation:",t or f)
+print("logical NOT operation:",not t)
+print("logical XOR operation:",t != f)
